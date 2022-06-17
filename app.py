238a785e21a4bd5736cf11f547a4372c0f86f729
Arinjay Saraf <arinjaysaraf@gmail.com>
2022-06-17 14:11:59 +0530

    flask api

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from typing import Union
+from flask import Flask, render_template
 
-from fastapi import FastAPI
+app = Flask(__name__)
 
-app = FastAPI()
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    app.run(debug=True)
