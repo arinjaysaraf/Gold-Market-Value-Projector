5c1e9c4b41ccaa33a61ad3511d793407c123e5f6
Arinjay Saraf <arinjaysaraf@gmail.com>
2022-06-20 23:25:20 +0530

    frontend changes

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from requests import request
+import utils
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/predict/', methods = ['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        Open = request.form.get['Open']
+        High = request.form.get['High']
+        Low = request.form.get['Low']
+        Volume = request.form.get['Volume']
+    prediction = utils.preprocess(Open, High, Low, Volume)
+    return render_template('index.html', prediction_text=prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
